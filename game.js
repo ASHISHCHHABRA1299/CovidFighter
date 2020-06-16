7eebc18f6faaebb67df44f38efe70ecddf553f1e
hp <chhabraashish123@gmail.com>
2020-06-16 16:35:13 +0530

    Enemies added

--- game.js
+++ game.js
@@ -2,6 +2,10 @@ function load_images()
 {
     //player image
     //virus,gem image
+    enemy_image=new Image();
+    enemy_image.src="Assets/v2.png";
+    enemy_image1=new Image();
+    enemy_image1.src="Assets/v1.png";
     
 }
 function init()
@@ -15,31 +19,53 @@ function init()
     //requires a string it is 2d canvas on which we render something(pen to draw)
     pen =canvas.getContext('2d');
     
-    box={
-        x:150,
-        y:50,
-        w:60,
-        h:60,
-        speed:20,
-    };
+    e1 = {
+		x : 150,
+		y : 50,
+		w : 60,
+		h : 60,
+		speed : 20,
+	};
+	e2 = {
+		x : 300,
+		y : 150,
+		w : 60,
+		h : 60,
+		speed : 30,
+	};
+	e3 = {
+		x : 450,
+		y : 20,
+		w : 60,
+		h : 60,
+		speed : 40,
+	};
+    
+    enemy = [e1,e2,e3];
 }
 
 function draw()
 {
     //clear the canvas area for the old frame
     pen.clearRect(0,0,W,H);
-    pen.fillStyle="red";
-    pen.fillRect(box.x,box.y,box.w,box.h);  
+    for(let i=0;i<enemy.length;i++){
+       if(i%2==0)
+           pen.drawImage(enemy_image,enemy[i].x,enemy[i].y,enemy[i].w,enemy[i].h);
+       else
+           pen.drawImage(enemy_image1,enemy[i].x,enemy[i].y,enemy[i].w,enemy[i].h);
+    } 
 }
 
 function update()
 {
     //move the box downwards
-    box.y+=box.speed;
-    if(box.y>=H-box.h||box.y<0)
-       {    
-           box.speed*=-1;
-       }
+     //update each enemy by same logic
+    for(let i=0;i<enemy.length;i++){
+        enemy[i].y += enemy[i].speed;
+        if(enemy[i].y>H-enemy[i].h || enemy[i].y <0){
+            enemy[i].speed *= -1;
+        }   
+    }
 }
 
 function gameloop()
