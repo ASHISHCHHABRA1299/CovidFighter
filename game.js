1a6c6f80f4957999f589fce7b760c5b1045fd7bd
hp <chhabraashish123@gmail.com>
2020-06-16 17:14:51 +0530

    Player movement Updated

--- game.js
+++ game.js
@@ -6,6 +6,10 @@ function load_images()
     enemy_image.src="Assets/v2.png";
     enemy_image1=new Image();
     enemy_image1.src="Assets/v1.png";
+    player_image=new Image();
+    player_image.src="Assets/superhero.png"
+    gem_image = new Image;
+    gem_image.src = "Assets/gemm.png";
     
 }
 function init()
@@ -42,12 +46,47 @@ function init()
 	};
     
     enemy = [e1,e2,e3];
+    player = {
+		x : 20,
+		y : H/2,
+		w : 60,
+		h : 60,
+		speed : 20,
+        moving  : false,
+        health : 100,
+	};
+    
+	gem = {
+		x : W-100,
+		y : H/2,
+		w : 60,
+		h : 60,
+	};
+    
+    // event listener on canvas object 
+    canvas.addEventListener('mousedown',function(){
+        console.log('mouse Pressed');
+        player.moving=true;
+    });
+    
+    //When mouse is up and not pressed so it should be stopped
+     canvas.addEventListener('mouseup',function(){
+        console.log('mouse Pressed');
+        player.moving=false;
+    });
 }
 
 function draw()
 {
     //clear the canvas area for the old frame
     pen.clearRect(0,0,W,H);
+    //draw the player
+    pen.drawImage(player_image,player.x,player.y,player.w,player.h);
+    
+    //draw the gem
+    pen.drawImage(gem_image,gem.x,gem.y,gem.w,gem.h);
+    
+    //draw the enemies
     for(let i=0;i<enemy.length;i++){
        if(i%2==0)
            pen.drawImage(enemy_image,enemy[i].x,enemy[i].y,enemy[i].w,enemy[i].h);
@@ -58,6 +97,11 @@ function draw()
 
 function update()
 {
+    //check if the player is moving
+    if(player.moving==true)
+    {
+        player.x+=player.speed;        
+    }
     //move the box downwards
      //update each enemy by same logic
     for(let i=0;i<enemy.length;i++){
